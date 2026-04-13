0d525b351c60206f34e771a936b6723792534d1a
AntonMoroz91 <140000779+AntonMoroz91@users.noreply.github.com>
2026-04-13 12:19:21 +0300

    Update filter_page.py

--- pages/filter_page.py
+++ pages/filter_page.py
@@ -1,21 +1,24 @@
+import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from .base_page import BasePage
 
 class FilterPage(BasePage):
     # Локаторы
-    LOCATION_FILTER = (By.XPATH, "/html/body/main/div/section[2]/div/div/aside/div/div[2]/form/div[5]/p/button")
-    OPTION_MOSCOW = (By.XPATH, "//label[contains(text(), 'Москва')]")
+    LOCATION_FILTER = (By.XPATH, "//button[contains(text(), 'Место проведения')]")
+    MOSCOW_CHECKBOX = (By.XPATH, "//label[contains(text(), 'Москва')]")
+    SHOW_MORE_BUTTON = (By.XPATH, "//button[contains(text(), 'Показать больше')]")
 
     def open_location_filter(self):
-        loc = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.LOCATION_FILTER)
-        )
-        loc.click()
+        loc = self.find_element(self.LOCATION_FILTER)
+        self.driver.execute_script("arguments[0].click();", loc)
+        time.sleep(1)
 
     def select_moscow(self):
-        moscow = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.OPTION_MOSCOW)
-        )
-        moscow.click()
+        moscow = self.find_element(self.MOSCOW_CHECKBOX)
+        self.driver.execute_script("arguments[0].click();", moscow)
+        time.sleep(1)
+
+    def click_show_more(self):
+        more = self.find_element(self.SHOW_MORE_BUTTON)
+        more.click()
+        time.sleep(1)

68808e215cb58f39e5ad1f007e6c34131493fdb1
AntonMoroz91 <140000779+AntonMoroz91@users.noreply.github.com>
2026-04-13 18:19:48 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,26 +1,16 @@
 import pytest
-import time
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def browser():
-    firefox_options = Options()
+    chrome_options = Options()
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--disable-notifications")
 
-    # Только самое важное
-    firefox_options.add_argument("--private")
-    firefox_options.set_preference("dom.webnotifications.enabled", False)
-    firefox_options.set_preference("dom.webdriver.enabled", False)
-    firefox_options.set_preference("useAutomationExtension", False)
-    firefox_options.set_preference("general.useragent.override",
-                                   "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0")
-
-    driver = webdriver.Firefox(options=firefox_options)
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
-
     yield driver
-
-    time.sleep(3)
     driver.quit()

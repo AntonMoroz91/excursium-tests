d5e7dc98e1fafb1cb654d3501d04d9c7c8924b1d
AntonMoroz91 <140000779+AntonMoroz91@users.noreply.github.com>
2026-04-13 19:48:22 +0300

    Update registration_page.py

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,3 +1,8 @@
+"""
+Страница регистрации нового пользователя.
+Содержит методы для заполнения формы, отправки и подтверждения кода.
+"""
+
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -13,49 +18,47 @@ class RegistrationPage(BasePage):
     CREATE_BUTTON = (By.XPATH, "//*[@id='registraion-btn']/span[2]")
 
     def open_login_icon(self):
-        icon = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.LOGIN_ICON)
-        )
+        """Нажать на иконку пользователя (открывает форму)"""
+        icon = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.LOGIN_ICON))
         icon.click()
         time.sleep(1)
 
     def open_registration_form(self):
+        """Переключиться на форму регистрации (вторая кнопка в контейнере)"""
         container = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.XPATH, "//*[@id='login-vue']/div/div[2]/div[3]/div[1]"))
         )
         buttons = container.find_elements(By.TAG_NAME, "button")
         if len(buttons) >= 2:
-            self.driver.execute_script("arguments[0].click();", buttons[1])  # JS-клик
+            # Клик по второй кнопке (Регистрация) через JS для надёжности
+            self.driver.execute_script("arguments[0].click();", buttons[1])
         else:
             raise Exception("Кнопка 'Регистрация' не найдена")
         time.sleep(1)
 
     def enter_email(self, email):
-        field = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.EMAIL_INPUT)
-        )
+        """Ввести email в поле формы регистрации"""
+        field = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.EMAIL_INPUT))
         field.clear()
         field.send_keys(email)
 
     def enter_password(self, password):
-        field = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located(self.PASSWORD_INPUT)
-        )
+        """Ввести пароль в поле формы регистрации"""
+        field = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(self.PASSWORD_INPUT))
         field.clear()
         field.send_keys(password)
 
     def accept_terms(self):
-        checkbox = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.TERMS_CHECKBOX)
-        )
-        self.driver.execute_script("arguments[0].click();", checkbox)  # JS-клик
+        """Поставить галочку согласия с условиями (клик через JS)"""
+        checkbox = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.TERMS_CHECKBOX))
+        self.driver.execute_script("arguments[0].click();", checkbox)
 
     def click_create_account(self):
-        btn = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(self.CREATE_BUTTON)
-        )
-        self.driver.execute_script("arguments[0].click();", btn)  # JS-клик
+        """Нажать кнопку 'Создать аккаунт' (клик через JS)"""
+        btn = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.CREATE_BUTTON))
+        self.driver.execute_script("arguments[0].click();", btn)
         time.sleep(2)
 
     def is_registration_successful(self):
+        """Проверка успешной регистрации: URL должен содержать 'startup'"""
         return "startup" in self.driver.current_url

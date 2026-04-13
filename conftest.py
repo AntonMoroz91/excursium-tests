707cb57303e0ad99afa83e4afa975a813f40647c
AntonMoroz91 <140000779+AntonMoroz91@users.noreply.github.com>
2026-04-13 12:24:15 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
@@ -8,21 +9,18 @@ from webdriver_manager.firefox import GeckoDriverManager
 def browser():
     firefox_options = Options()
 
-    # Режим инкогнито (приватный)
+    # Только самое важное
     firefox_options.add_argument("--private")
-
-    # Отключаем уведомления
     firefox_options.set_preference("dom.webnotifications.enabled", False)
-
-    # Отключаем загрузку изображений (ускоряет тесты)
-    firefox_options.set_preference("permissions.default.image", 2)
-
-    # Маскировка WebDriver (чтобы не блокировали)
     firefox_options.set_preference("dom.webdriver.enabled", False)
     firefox_options.set_preference("useAutomationExtension", False)
+    firefox_options.set_preference("general.useragent.override",
+                                   "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:136.0) Gecko/20100101 Firefox/136.0")
 
     driver = webdriver.Firefox(options=firefox_options)
     driver.maximize_window()
 
     yield driver
+
+    time.sleep(3)
     driver.quit()

7637c90ca2abd3d41026d4ec77be1b8b1d469c83
AntonMoroz91 <140000779+AntonMoroz91@users.noreply.github.com>
2026-04-13 12:21:35 +0300

    Update test_registration.py

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,32 +1,44 @@
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from pages.main_page import MainPage
 from pages.registration_page import RegistrationPage
-from faker import Faker
 
-fake = Faker()
-
-
-def test_registration(browser):
-    main_page = MainPage(browser)
-    main_page.open("https://excursium.com")
-    main_page.click(main_page.USER_ICON)
-    time.sleep(2)
-
-    # Клик по вкладке "Регистрация" внутри контейнера
-    reg_tab = WebDriverWait(browser, 10).until(
-        EC.element_to_be_clickable(
-            (By.XPATH, "//*[@id='login-vue']/div/div[2]/div[3]//button[contains(text(),'Регистрация')]"))
-    )
-    browser.execute_script("arguments[0].click();", reg_tab)
-    time.sleep(2)
-
+def test_registration_new_user(browser):
     reg_page = RegistrationPage(browser)
-    email = fake.email()
-    password = "Qwerty123"
-    reg_page.register(email, password)
+    reg_page.open("https://excursium.com")
+    time.sleep(2)
+
+    email = "autotest.qa11@gmail.com"
+    password = "Zenitpiter1925!!!!"
+
+    print(f"\n📧 Регистрируем почту: {email}")
+
+    reg_page.open_login_icon()
+    time.sleep(1)
+
+    reg_page.open_registration_form()
+    time.sleep(1)
+
+    reg_page.enter_email(email)
+    reg_page.enter_password(password)
+    reg_page.accept_terms()
+
+    reg_page.click_create_account()
     time.sleep(3)
 
-    assert "excursium" in browser.current_url
+    print(f"\n📨 Код отправлен на {email}")
+    code = input("🔢 Введите 4-значный код из письма: ")
+
+    for i, digit in enumerate(code, start=1):
+        field = browser.find_element(By.XPATH, f"//*[@id='login-vue']/div/div[2]/div[6]/div[2]/input[{i}]")
+        field.send_keys(digit)
+        time.sleep(0.3)
+
+    confirm_btn = browser.find_element(By.XPATH, "//*[@id='checkCode-btn']/span[2]")
+    confirm_btn.click()
+    time.sleep(3)
+
+    # Проверка: мы на странице Startup (регистрация успешна)
+    assert "Startup" in browser.current_url, "Регистрация не удалась"
+
+    browser.save_screenshot("screenshots/TC-02_registration_success.png")
+    print("\n✅ Регистрация успешно завершена! Тест PASSED.")

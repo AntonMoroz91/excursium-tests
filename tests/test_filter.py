912e0620d2be9a63d96ca211b40f2bf3e74e6d18
AntonMoroz91 <140000779+AntonMoroz91@users.noreply.github.com>
2026-04-13 19:50:18 +0300

    Update test_filter.py

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,3 +1,7 @@
+"""
+Тест фильтрации экскурсий по месту проведения (Москва).
+"""
+
 import time
 from pages.filter_page import FilterPage
 
@@ -6,10 +10,11 @@ def test_filter(browser):
     filter_page.open("https://excursium.com/ekskursii-dlya-shkolnikov/list")
     time.sleep(2)
 
-    filter_page.open_location_filter()
-    filter_page.select_moscow()
+    filter_page.open_location_filter()   # открываем фильтр "Место проведения"
+    filter_page.select_moscow()          # выбираем "Москва"
     time.sleep(2)
 
+    # Проверяем, что в URL или на странице появилась Москва
     assert "moscow" in browser.current_url.lower() or "Москва" in browser.page_source
 
     browser.save_screenshot("screenshots/TC-06_filter_moscow.png")
